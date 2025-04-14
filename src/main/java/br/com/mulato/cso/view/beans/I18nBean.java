96eb55bc5b80aa7172727b23acb9b85f3c1f1097
Christian Mulato <chmulato@hotmail.com>
2025-04-14 17:53:15 -0300

    Update I18nBean.java

--- src/main/java/br/com/mulato/cso/view/beans/I18nBean.java
+++ src/main/java/br/com/mulato/cso/view/beans/I18nBean.java
@@ -11,7 +11,6 @@ import javax.faces.event.ActionEvent;
 /**
  * @author Christian Mulato
  * @date Feb/07th/2011
- * @job www.unisistemas.com.br
  */
 @ManagedBean(name = "i18nMB")
 @SessionScoped
@@ -19,7 +18,7 @@ public class I18nBean implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
-	// Internazionalização p/ o português
+	// InternazionalizaÃ§Ã£o p/ o portuguÃªs
 	private String locale = "pt_BR";
 
 	public String getLocale() {
