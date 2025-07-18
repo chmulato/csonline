7116e2cc6facc1fccb45e28dc8b5d5793366e808
Christian Mulato <chrvlamulato@rd.com.br>
2025-07-18 14:11:11 -0300

    Refactor: Improve code formatting and fix comments in InterfaceSQL and BusinessServiceImpl

--- src/main/java/br/com/mulato/cso/service/impl/BusinessServiceImpl.java
+++ src/main/java/br/com/mulato/cso/service/impl/BusinessServiceImpl.java
@@ -13,24 +13,19 @@ import br.com.mulato.cso.model.PriceVO;
 import br.com.mulato.cso.service.BusinessService;
 
 public class BusinessServiceImpl
-    implements BusinessService
-{
+		implements BusinessService {
 
 	private static final long serialVersionUID = 1L;
 
 	private final static Logger LOGGER = Logger.getLogger(BusinessServiceImpl.class);
 
 	@Override
-	public List<BusinessVO> listAllBusiness () throws WebException
-	{
+	public List<BusinessVO> listAllBusiness() throws WebException {
 		List<BusinessVO> list = null;
-		LOGGER.info("Listar todos os neg骳ios.");
-		try
-		{
+		LOGGER.info("Listar todos os neg贸cios.");
+		try {
 			list = FactoryDAO.getInstancia().getBusinessDAO().listAll();
-		}
-		catch (final DAOException e)
-		{
+		} catch (final DAOException e) {
 			LOGGER.error("Service error: " + e.getMessage());
 			throw new WebException(e.getMessage());
 		}
@@ -38,94 +33,71 @@ public class BusinessServiceImpl
 	}
 
 	@Override
-	public void save (final BusinessVO business) throws WebException
-	{
-		if (business == null)
-		{
-			throw new WebException("Informe neg骳io.");
+	public void save(final BusinessVO business) throws WebException {
+		if (business == null) {
+			throw new WebException("Informe neg贸cio.");
 		}
 
-		if (business.getName() == null)
-		{
-			throw new WebException("Informe nome do neg骳io!");
+		if (business.getName() == null) {
+			throw new WebException("Informe nome do neg贸cio!");
 		}
 
-		if (business.getRole() == null)
-		{
-			throw new WebException("Informe o perfil de neg骳io!");
+		if (business.getRole() == null) {
+			throw new WebException("Informe o perfil de neg贸cio!");
 		}
 
-		if (!business.getRole().equals("BUSINESS"))
-		{
-			throw new WebException("Informe o perfil de neg骳io!");
+		if (!business.getRole().equals("BUSINESS")) {
+			throw new WebException("Informe o perfil de neg贸cio!");
 		}
 
-		if (business.getLogin() == null)
-		{
-			throw new WebException("Informe login do neg骳io!");
+		if (business.getLogin() == null) {
+			throw new WebException("Informe login do neg贸cio!");
 		}
 
-		if (business.getLogin().getLogin() == null)
-		{
-			throw new WebException("Informe login do neg骳io!");
+		if (business.getLogin().getLogin() == null) {
+			throw new WebException("Informe login do neg贸cio!");
 		}
 
-		if (business.getId() == null)
-		{
-			if (business.getLogin().getPassword() == null)
-			{
-				throw new WebException("Informe senha do neg骳io!");
+		if (business.getId() == null) {
+			if (business.getLogin().getPassword() == null) {
+				throw new WebException("Informe senha do neg贸cio!");
 			}
-			if (business.getLogin().getRepeat() == null)
-			{
-				throw new WebException("Repita senha do neg骳io!");
+			if (business.getLogin().getRepeat() == null) {
+				throw new WebException("Repita senha do neg贸cio!");
 			}
-			if (!business.getLogin().getPassword().equals(business.getLogin().getRepeat()))
-			{
+			if (!business.getLogin().getPassword().equals(business.getLogin().getRepeat())) {
 				throw new WebException("Repita senha corretamente!");
 			}
 		}
 
-		if (business.getEmail() == null)
-		{
-			throw new WebException("Informe email do neg骳io!");
+		if (business.getEmail() == null) {
+			throw new WebException("Informe email do neg贸cio!");
 		}
 
-		try
-		{
-			if (business.getId() == null)
-			{
-				LOGGER.info("Salvar neg骳io.");
+		try {
+			if (business.getId() == null) {
+				LOGGER.info("Salvar neg贸cio.");
 				FactoryDAO.getInstancia().getBusinessDAO().insert(business);
-			}
-			else
-			{
-				LOGGER.info("Atualizar neg骳io.");
+			} else {
+				LOGGER.info("Atualizar neg贸cio.");
 				FactoryDAO.getInstancia().getBusinessDAO().update(business);
 			}
-		}
-		catch (final DAOException e)
-		{
+		} catch (final DAOException e) {
 			LOGGER.error("Service error: " + e.getMessage());
 			throw new WebException(e.getMessage());
 		}
 	}
 
 	@Override
-	public BusinessVO find (final Integer idBusiness) throws WebException
-	{
+	public BusinessVO find(final Integer idBusiness) throws WebException {
 		BusinessVO business = null;
-		if ((idBusiness == null) || (idBusiness <= 0))
-		{
-			throw new WebException("Informe id neg骳io.");
+		if ((idBusiness == null) || (idBusiness <= 0)) {
+			throw new WebException("Informe id neg贸cio.");
 		}
-		LOGGER.info("Buscar neg骳io.");
-		try
-		{
+		LOGGER.info("Buscar neg贸cio.");
+		try {
 			business = FactoryDAO.getInstancia().getBusinessDAO().find(idBusiness);
-		}
-		catch (final DAOException e)
-		{
+		} catch (final DAOException e) {
 			LOGGER.error("Service error: " + e.getMessage());
 			throw new WebException(e.getMessage());
 		}
@@ -133,44 +105,33 @@ public class BusinessServiceImpl
 	}
 
 	@Override
-	public void delete (final Integer idBusiness) throws WebException
-	{
-		if ((idBusiness == null) || (idBusiness <= 0))
-		{
-			throw new WebException("Informe id neg骳io.");
+	public void delete(final Integer idBusiness) throws WebException {
+		if ((idBusiness == null) || (idBusiness <= 0)) {
+			throw new WebException("Informe id neg贸cio.");
 		}
-		LOGGER.info("Deletar neg骳io.");
-		try
-		{
+		LOGGER.info("Deletar neg贸cio.");
+		try {
 			FactoryDAO.getInstancia().getBusinessDAO().delete(idBusiness);
-		}
-		catch (final DAOException e)
-		{
+		} catch (final DAOException e) {
 			LOGGER.error("Service error: " + e.getMessage());
 			throw new WebException(e.getMessage());
 		}
 	}
 
 	@Override
-	public BusinessVO findBusinessByCustomerId (final Integer idCustomer) throws WebException
-	{
+	public BusinessVO findBusinessByCustomerId(final Integer idCustomer) throws WebException {
 		BusinessVO business = null;
-		if (idCustomer == null)
-		{
+		if (idCustomer == null) {
 			throw new WebException("Informe id cliente.");
 		}
-		if (idCustomer <= 0)
-		{
+		if (idCustomer <= 0) {
 			throw new WebException("Informe id cliente.");
 		}
-		try
-		{
+		try {
 			final CustomerVO customer = new CustomerVO();
 			customer.setId(idCustomer);
 			business = FactoryDAO.getInstancia().getBusinessDAO().findCustomerBusiness(customer);
-		}
-		catch (final DAOException e)
-		{
+		} catch (final DAOException e) {
 			LOGGER.error("Service error: " + e.getMessage());
 			throw new WebException(e.getMessage());
 		}
@@ -178,25 +139,19 @@ public class BusinessServiceImpl
 	}
 
 	@Override
-	public BusinessVO findBusinessByCourierId (final Integer idCourier) throws WebException
-	{
+	public BusinessVO findBusinessByCourierId(final Integer idCourier) throws WebException {
 		BusinessVO business = null;
-		if (idCourier == null)
-		{
+		if (idCourier == null) {
 			throw new WebException("Informe id entregador.");
 		}
-		if (idCourier <= 0)
-		{
+		if (idCourier <= 0) {
 			throw new WebException("Informe id entregador.");
 		}
-		try
-		{
+		try {
 			final CourierVO courier = new CourierVO();
 			courier.setId(idCourier);
 			business = FactoryDAO.getInstancia().getBusinessDAO().findCourierBusiness(courier);
-		}
-		catch (final DAOException e)
-		{
+		} catch (final DAOException e) {
 			LOGGER.error("Service error: " + e.getMessage());
 			throw new WebException(e.getMessage());
 		}
@@ -204,24 +159,18 @@ public class BusinessServiceImpl
 	}
 
 	@Override
-	public List<PriceListVO> listPriceListBusiness (final Integer ibBusiness) throws WebException
-	{
+	public List<PriceListVO> listPriceListBusiness(final Integer ibBusiness) throws WebException {
 		List<PriceListVO> list = null;
-		if (ibBusiness == null)
-		{
-			throw new WebException("Informe id neg骳io.");
+		if (ibBusiness == null) {
+			throw new WebException("Informe id neg贸cio.");
 		}
-		if (ibBusiness <= 0)
-		{
-			throw new WebException("Informe id neg骳io.");
+		if (ibBusiness <= 0) {
+			throw new WebException("Informe id neg贸cio.");
 		}
-		LOGGER.info("Pesquisar listas de pre鏾s do neg骳io.");
-		try
-		{
+		LOGGER.info("Pesquisar listas de pre莽os do neg贸cio.");
+		try {
 			list = FactoryDAO.getInstancia().getPriceDAO().listAllPriceListBusiness(ibBusiness);
-		}
-		catch (final DAOException e)
-		{
+		} catch (final DAOException e) {
 			LOGGER.error("Service error: " + e.getMessage());
 			throw new WebException(e.getMessage());
 		}
@@ -229,24 +178,18 @@ public class BusinessServiceImpl
 	}
 
 	@Override
-	public List<PriceVO> listAllPriceBusiness (final Integer idBusiness) throws WebException
-	{
+	public List<PriceVO> listAllPriceBusiness(final Integer idBusiness) throws WebException {
 		List<PriceVO> list = null;
-		if (idBusiness == null)
-		{
-			throw new WebException("Informe id neg骳io.");
+		if (idBusiness == null) {
+			throw new WebException("Informe id neg贸cio.");
 		}
-		if (idBusiness <= 0)
-		{
-			throw new WebException("Informe id neg骳io.");
+		if (idBusiness <= 0) {
+			throw new WebException("Informe id neg贸cio.");
 		}
-		LOGGER.info("Pesquisar tabela de pre鏾s do neg骳io.");
-		try
-		{
+		LOGGER.info("Pesquisar tabela de pre莽os do neg贸cio.");
+		try {
 			list = FactoryDAO.getInstancia().getPriceDAO().listAllPriceBusiness(idBusiness);
-		}
-		catch (final DAOException e)
-		{
+		} catch (final DAOException e) {
 			LOGGER.error("Service error: " + e.getMessage());
 			throw new WebException(e.getMessage());
 		}
@@ -254,32 +197,24 @@ public class BusinessServiceImpl
 	}
 
 	@Override
-	public List<PriceVO> listAllPriceBusinessByTable (final Integer idBusiness, final String table) throws WebException
-	{
+	public List<PriceVO> listAllPriceBusinessByTable(final Integer idBusiness, final String table) throws WebException {
 		List<PriceVO> list = null;
-		if (idBusiness == null)
-		{
-			throw new WebException("Informe id neg骳io.");
+		if (idBusiness == null) {
+			throw new WebException("Informe id neg贸cio.");
 		}
-		if (idBusiness <= 0)
-		{
-			throw new WebException("Informe id neg骳io.");
+		if (idBusiness <= 0) {
+			throw new WebException("Informe id neg贸cio.");
 		}
-		if (table == null)
-		{
+		if (table == null) {
 			throw new WebException("Informe nome da tabela.");
 		}
-		if (table.equals(""))
-		{
+		if (table.equals("")) {
 			throw new WebException("Informe nome da tabela.");
 		}
-		LOGGER.info("Pesquisar tabela de pre鏾s do neg骳io.");
-		try
-		{
+		LOGGER.info("Pesquisar tabela de pre莽os do neg贸cio.");
+		try {
 			list = FactoryDAO.getInstancia().getPriceDAO().listAllPriceBusinessByTable(idBusiness, table);
-		}
-		catch (final DAOException e)
-		{
+		} catch (final DAOException e) {
 			LOGGER.error("Service error: " + e.getMessage());
 			throw new WebException(e.getMessage());
 		}
@@ -287,19 +222,14 @@ public class BusinessServiceImpl
 	}
 
 	@Override
-	public void deleteBusinessValues (final Integer idBusiness) throws WebException
-	{
-		if ((idBusiness == null) || (idBusiness <= 0))
-		{
-			throw new WebException("Informe id neg骳io.");
+	public void deleteBusinessValues(final Integer idBusiness) throws WebException {
+		if ((idBusiness == null) || (idBusiness <= 0)) {
+			throw new WebException("Informe id neg贸cio.");
 		}
-		LOGGER.info("Deletar todas as listas de pre鏾s do neg骳io.");
-		try
-		{
+		LOGGER.info("Deletar todas as listas de pre莽os do neg贸cio.");
+		try {
 			FactoryDAO.getInstancia().getPriceDAO().deleteBusinessValues(idBusiness);
-		}
-		catch (final DAOException e)
-		{
+		} catch (final DAOException e) {
 			LOGGER.error("Service error: " + e.getMessage());
 			throw new WebException(e.getMessage());
 		}
